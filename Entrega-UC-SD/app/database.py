ca51778870c285d5d293a01fecfc8ec304ef5ee3
Guilherme Gonçalves <112209360+GuilhermeXGoncalves@users.noreply.github.com>
2023-12-07 11:03:26 -0300

    Atualizações de organização

--- Entrega-UC-SD/app/database.py
+++
@@ -1,5 +0,0 @@
-import sqlite3
-conexao = sqlite3.connect('loja.db', check_same_thread=False)  
-cursor = conexao.cursor()
-
-
--- app/server.py
+++ app/server.py
@@ -1,4 +1,4 @@
-import estoque, client, venda
+import estoque, client
 from flask import Flask, jsonify, render_template, request
 
 
@@ -86,23 +86,6 @@ def deleta_client_db(id):
     return jsonify(cliente)
 
 # CADASTRAR CLIENTE
-
-
-
-# REGISTRAR VENDA
-@app.route('/vendas/add', methods=['POST'])
-def registrar_venda():
-    data = request.get_json()
-    id_cliente = data['id_cliente']
-    produtos = data['produtos']
-    venda.registrarVenda(id_cliente, produtos)
-    return ('OK')
-
-# CONSULTAR TODAS AS VENDAS
-@app.route('/vendas', methods=['GET'])
-def obter_todas_vendas():
-    vendas = venda.obterTodasVendas()
-    return jsonify(vendas)
 
 
 if __name__ == "__main__":
